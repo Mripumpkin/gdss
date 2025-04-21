c5b5bbc7bec17b94777114f4d0b7c984ffa218bb
Mripumpkin <847906708@qq.com>
2025-04-21 17:07:41 +0800

    :sparkles: panic处理：打印堆栈信息

---
+++ pkg/util.go
@@ -0,0 +1,13 @@
+package pkg
+
+import "runtime"
+
+const (
+	defaultStackSize = 4096
+)
+
+func GetCurrentGoroutineStack() string {
+	var buf [defaultStackSize]byte
+	n := runtime.Stack(buf[:], false)
+	return string(buf[:n])
+}
