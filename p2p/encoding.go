2df1e85ee21f9a9c463e965d38e31f412dc28a9c
Mripumpkin <847906708@qq.com>
2025-05-02 20:16:26 +0800

    :recycle: store修改,添加流类型数据处理

--- p2p/encoding.go
+++ p2p/encoding.go
@@ -18,7 +18,16 @@ func (d *GOBDecoder) Decode(r io.Reader, msg *RPC) error {
 type DefaultDecoder struct{}
 
 func (dec DefaultDecoder) Decode(r io.Reader, msg *RPC) error {
-	buf := make([]byte, 1024)
+	peekBuf := make([]byte, 1)
+	if _, err := r.Read(peekBuf); err != nil {
+		return err
+	}
+	stream := peekBuf[0] == IncomingStream
+	if stream {
+		msg.Stream = true
+		return nil
+	}
+	buf := make([]byte, 1028)
 	n, err := r.Read(buf)
 	if err != nil {
 		return err
